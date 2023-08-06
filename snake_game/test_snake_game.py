17951409247e54951980dee52952a2e94e1379a4
Emma O'Leary <oleemm31@evergreen.edu>
2023-08-06 09:39:57 -0700

    Added an example test in preparation for test-driven development practice.

---
+++ snake_game/test_snake_game.py
@@ -0,0 +1,11 @@
+import unittest
+
+class TestSnakeGame(unittest.TestCase):
+    def example(self):
+        self.assertEqual(2 + 2, 4)
+        # assertEqual() takes 2 arguments:
+            # The first argument is the question.
+            # The second argument is the expected answer.
+
+if __name__ == '__main__':
+    unittest.main()
